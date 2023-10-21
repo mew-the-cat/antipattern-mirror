90e041e60e360e33ba0fbe6a04ac170a0219ff08
me (ich) <georg+git@hostpath.de>
2023-10-21 11:35:12 +0200

    fix paths

--- backend/database/models/sequelize.ts
+++ backend/database/models/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/db.config.json')[env];
+const config = require('../../../database/config/db.config.json')[env];
 
 //const  sequelize = new Sequelize(config.database, config.username, config.password, config);
 
